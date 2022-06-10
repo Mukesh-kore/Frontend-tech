fb1c37c5b71917e6cbc636395dd051d613c0d6f9
Kore Mukesh Madhav <koremukesh9@gmail.com>
2022-06-10 17:16:40 +0530

    initial commit

---
+++ javascript/index.js
@@ -0,0 +1,15 @@
+let z;
+console.log(z);
+
+var y="ram";
+
+console.log(y);
+
+function getname() {
+    var x=4;
+    const y="krishna";
+    console.log(x);
+    console.log(y);
+    return 0;    
+}
+getname();
